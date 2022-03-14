43cf0876ce11745803f7030ac0e0107849776e42
paval_miatlitski <paval.escoba@gmail.com>
2022-03-15 01:50:54 +0300

    basic set up

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container fluid>
+    <v-app-bar color="orange" dark>
+      <v-spacer />
+      <v-btn outlined class="mr-4">Main</v-btn>
+      <v-btn outlined>Photos</v-btn>
+    </v-app-bar>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
