34954be4001e8ae561af2c8445222b2dddbd6dfd
paval_miatlitski <paval.escoba@gmail.com>
2022-03-16 02:19:02 +0300

    router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <Navbar />
+    <router-view />
   </v-app>
 </template>
 
@@ -12,9 +13,5 @@ export default {
   components: {
     Navbar,
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
