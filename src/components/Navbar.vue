657c1ae6067a23f994a72a62caee1cd30b346fa6
paval_miatlitski <paval.escoba@gmail.com>
2022-03-17 02:45:35 +0300

    photo component + photos row

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,8 +2,12 @@
   <v-container fluid>
     <v-app-bar color="orange" dark>
       <v-spacer />
-      <v-btn outlined class="mr-4">Main</v-btn>
-      <v-btn outlined>Photos</v-btn>
+      <v-btn outlined class="mr-4">
+        <router-link to="/">Main page</router-link>
+      </v-btn>
+      <v-btn outlined>
+        <router-link to="/photo">Photos</router-link>
+      </v-btn>
     </v-app-bar>
   </v-container>
 </template>
--- src/pages/PhotosPage.vue
+++ src/pages/PhotosPage.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>Photos</div>
+  <v-container>
+    <v-row>
+      <Photo v-for="photo in photos" :key="photo.id" :photo="photo" />
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {};
+import Photo from '@/components/photo/Photo.vue';
+export default {
+  components: {
+    Photo,
+  },
+  data: () => ({
+    photos: [],
+  }),
+  mounted() {
+    this.fetchToDo();
+  },
+  methods: {
+    fetchToDo() {
+      this.axios
+        .get('https://jsonplaceholder.typicode.com/photos?_limit=10')
+        .then((response) => (this.photos = response.data));
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
