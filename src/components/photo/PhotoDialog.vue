9fc916bce55fbcd777ae74ed09dc090ead8474c6
paval_miatlitski <paval.escoba@gmail.com>
2022-03-21 03:01:40 +0300

    WIP: dialog pop-up

---
+++ src/components/photo/PhotoDialog.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-dialog v-model="value" max-width="600">
+    <v-card>
+      <v-card-title>{{ photo.title }} </v-card-title>
+      <v-card-text>
+        <v-img :src="photo.url" />
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  propd: {
+    photo: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({
+    dialogVisible: false,
+  }),
+  created() {
+    this.dialogVisible = this.value;
+  },
+  watch: {
+    value(newValue) {
+      this.dialogVisible = newValue;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/PhotosPage.vue
+++ src/pages/PhotosPage.vue
@@ -1,19 +1,33 @@
 <template>
   <v-container>
+    <PhotoForm @addPhoto="addingPhoto" />
     <v-row>
-      <Photo v-for="photo in photos" :key="photo.id" :photo="photo" />
+      <Photo
+        v-for="photo in photos"
+        :key="photo.id"
+        :photo="photo"
+        @openPhoto="openPhoto"
+      />
     </v-row>
+    <PhotoDialog :photo="currentPhoto" :v-model="dialogVisible" />
   </v-container>
 </template>
 
 <script>
 import Photo from '@/components/photo/Photo.vue';
+import PhotoForm from '@/components/photo/PhotoForm.vue';
+import PhotoDialog from '@/components/photo/PhotoDialog.vue';
+
 export default {
   components: {
     Photo,
+    PhotoForm,
+    PhotoDialog,
   },
   data: () => ({
     photos: [],
+    currentPhoto: {},
+    value: false,
   }),
   mounted() {
     this.fetchToDo();
@@ -23,6 +37,14 @@ export default {
       this.axios
         .get('https://jsonplaceholder.typicode.com/photos?_limit=10')
         .then((response) => (this.photos = response.data));
+    },
+    addingPhoto(photo) {
+      this.photos.push(photo);
+    },
+    openPhoto(photo) {
+      console.log(photo);
+      this.currentPhoto = photo;
+      this.dialogVisible = true;
     },
   },
 };
